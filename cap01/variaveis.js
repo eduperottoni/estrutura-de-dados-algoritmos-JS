5f374f36e46d6d53806e26309357e6dc88b57060
Eduardo Perottoni <edu.perottoni@gmail.com>
2022-08-22 23:54:39 -0300

    Definindo estrutura de pastas

---
+++ cap01/variaveis.js
@@ -0,0 +1,10 @@
+//Declaração de variáveis:
+var num = 3;
+/* TIPAGEM DINÂMICA
+O tipo da variável é definido em tempo de execução */
+console.log(`${num} --> ${typeof(num)}`);
+//Atribuição de variável:
+num = 5.6;
+console.log(`${num} --> ${typeof(num)}`);
+
+
